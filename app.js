581f55a0837f1794531b71c2a353dfbce5d86f83
Love Kumar <lovekumar714283@gmail.com>
2025-08-07 02:40:50 +0530

    Updated code with working backend

--- app.js
+++ app.js
@@ -24,12 +24,11 @@ require("dotenv").config();
 
 // Configure the database connection
 const db = mysql.createConnection({
-  connectionLimit: 10,
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.DATABASE_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
